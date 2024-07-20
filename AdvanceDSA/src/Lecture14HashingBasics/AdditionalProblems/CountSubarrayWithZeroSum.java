31d1aa0e9a9d1ec1b61efd070e682cbd1ef679db
ayush-kumar774 <ayushkumar1234@protonmail.com>
2024-07-20 22:56:14 +0530

    Added the code for assigment and additional assigment

--- AdvanceDSA/src/Lecture14HashingBasics/AdditionalProblems/CountSubarrayWithZeroSum.java
+++ AdvanceDSA/src/Lecture14HashingBasics/AdditionalProblems/CountSubarrayWithZeroSum.java
@@ -1,4 +1,35 @@
 package Lecture14HashingBasics.AdditionalProblems;
 
+import java.util.HashMap;
+
 public class CountSubarrayWithZeroSum {
+    private static int solve(int[] A) {
+        int MOD = 1000000007;
+        int n = A.length;
+
+        HashMap<Integer, Integer> prefixSumFreq = new HashMap<>();
+        int count = 0 ;
+        int prefixSum = 0 ;
+
+        prefixSumFreq.put(0, 1);
+
+        for (int j : A) {
+            prefixSum += j;
+
+            if (prefixSumFreq.containsKey(prefixSum)) {
+                count = (count + prefixSumFreq.get(prefixSum)) % MOD;
+            }
+
+            prefixSumFreq.put(prefixSum, prefixSumFreq.getOrDefault(prefixSum, 0) + 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, -1, -2, 2};
+        int[] B = {-1, 2, -1};
+
+        System.out.println(solve(A));
+        System.out.println(solve(B));
+    }
 }
